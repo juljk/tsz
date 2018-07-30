87251e413032bd27830b1ea44755c8a05fd902af
juljk <julikjulik22@gmail.com>
2018-07-30 11:36:26 +0300

    fix config

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  MONGODB_URI: 'mongodb://tsart:1q2w3e4r5t@ds129051.mlab.com:29051/tsartdb',
-  
+  MONGODB_URI: 'mongodb://tsart:1q2w3e4r5t@ds129051.mlab.com:29051/tsartdb',  
 };
